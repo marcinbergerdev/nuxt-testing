b111b3aec53d49828d95860a92eb3d7f7a565574
marcinbergerdev <marcin.berger95@op.pl>
2023-07-10 17:02:36 +0200

    feat: add scss and routing components

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app-container">
+    sad
+
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss"> 
+.app-container{
+  width: 100%;
+  height: 100vh;
+  background-color: rgb(116, 66, 66);
+}
+</style>
