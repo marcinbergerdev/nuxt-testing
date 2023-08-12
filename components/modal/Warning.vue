d6536781bbeec7e1a8fadf75d72aa589326ef3cc
marcinbergerdev <marcin.berger95@op.pl>
2023-08-12 16:51:18 +0200

    refactor: dialog center and set animation

--- components/modal/Warning.vue
+++ components/modal/Warning.vue
@@ -2,7 +2,7 @@
   <Teleport to="body">
     <Transition name="modal" mode="out-in">
       <div class="modal-container" v-if="isModal">
-        <dialog open>
+        <dialog class="modal-dialog" open>
           <button class="modal-close" @click="emit('close')">back</button>
           <h2 class="modal-header">warning!</h2>
           <p class="modal-description">
@@ -25,4 +25,65 @@ const emit = defineEmits<{
 }>();
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+@keyframes dialogAnimation {
+  0% {
+    opacity: 0;
+    scale: 0;
+  }
+  100% {
+    opacity: 1;
+    scale: 1;
+  }
+}
+
+.modal-container {
+  position: absolute;
+  top: 0;
+  z-index: 10;
+  width: 100%;
+  min-height: 100%;
+  background-color: rgba(#000000, 0.4);
+  display: flex;
+  align-items: center;
+}
+
+.modal-dialog {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem 0;
+
+  margin: 0 auto;
+  padding: 2rem;
+  width: min(40rem, 70%);
+
+  text-align: center;
+  border-radius: 1.5rem;
+  border: 1px solid var(--light-red);
+  animation: dialogAnimation 0.2s;
+}
+
+.modal-close {
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+  background-color: #151f2c;
+  color: white;
+  padding: 0.3rem;
+  border-radius: 0.4rem;
+
+  @media (width >= 1024px) {
+    &:hover {
+      cursor: pointer;
+    }
+  }
+}
+
+.modal-header {
+  font-size: 2.5rem;
+}
+
+.modal-description {
+  font-size: 1.3rem;
+}
+</style>
